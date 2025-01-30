963cbec673d60ad4168e5584335db23fe520fada
Clawwo <genzz760@gmail.com>
2025-01-30 19:46:40 +0700

    update file

---
+++ Dicoding/Program.js
@@ -0,0 +1,10 @@
+// 1. Percabangan If
+let umur = null //menciptakan kondisi
+//memberikan perintah sesuai dengan kondisi
+if (umur >= 18) {
+    console.log("Anda sudah dewasa");
+} else if (umur >= 10) {
+    console.log("Anda masih anak-anak");
+} else {
+    console.log("masukkan umur");
+}
